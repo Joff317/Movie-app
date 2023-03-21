883ed6e6b5e41bb4537494c1cb3b497df08ecaf9
Joff317 <j.freixanet@hotmail.com>
2023-03-21 13:00:46 +0100

    search design

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_MOVIE_URL = "https://api.themoviedb.org/3"
+
+export const API_KEY_MOVIE =`${API_MOVIE_URL}/movie/popular?api_key=`
+
+export const API_IMAGE = "https://image.tmdb.org/t/p/w500"
