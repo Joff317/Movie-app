2f96daf6441ec4a96d6f31f99ab69ca2a45bd668
Joff317 <j.freixanet@hotmail.com>
2023-03-22 15:10:39 +0100

    finish fav page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export const API_MOVIE_URL = "https://api.themoviedb.org/3";
 
+export const API_MOVIE_FAV = "https://api.themoviedb.org/3/movie/550?api_key=";
+
 export const API_KEY_MOVIE = `${API_MOVIE_URL}/movie/popular?api_key=`;
 
 export const API_GENRE_MOVIE = `${API_MOVIE_URL}/genre/movie/list?api_key=`;
--- src/pages/LikePage.js
+++ src/pages/LikePage.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import { API_MOVIE_FAV, API_MOVIE_URL } from "../config";
+import Card from "../components/Card";
 
 const LikePage = () => {
-   return (
-      <div>
-         <Header/>
-         {/* <h1>Page Like</h1> */}
+  const [listData, setListData] = useState([]);
+
+  const PUBLIC_KEY = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movie
+      ? window.localStorage.movie.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(API_MOVIE_URL + `/movie/${moviesId[i]}?api_key=` + PUBLIC_KEY)
+        .then((res) => setListData((listData) => [...listData, res.data]))
+        
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>Coups de Coeur</h2>
+      <div className="result">
+         {listData.length > 0 ? (
+            listData.map((movie) => <Card movie={movie} key={movie.id}/>)
+         ) : (
+            <h2>Aucun Coup de coeur pour le moment</h2>
+         )}
       </div>
-   );
+    </div>
+  );
 };
 
 export default LikePage;
