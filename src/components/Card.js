de1a51f32e6c7c2afcead7492e608deeb727f73f
Joff317 <j.freixanet@hotmail.com>
2023-03-22 13:52:53 +0100

    finish card_component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,89 @@
-import React from "react";
-import { API_IMAGE } from "../config";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { API_GENRE_MOVIE, API_IMAGE, API_MOVIE_URL } from "../config";
 
 const Card = ({ movie }) => {
-
   console.log(movie);
 
   const dateFormater = (date) => {
-    let [yy, mm, dd] = date.split("-")
-    return [dd, mm, yy].join("/")
-  }
+    let [yy, mm, dd] = date.split("-");
+    return [dd, mm, yy].join("/");
+  };
+
+  const PUBLIC_KEY = process.env.REACT_APP_API_KEY;
+
+
+  const genreFinder = () => {
+    let genreArray = [];
+    for (let i = 0; i < movie.genre_ids.length; i++) {
+      switch (movie.genre_ids[i]) {
+        case 28:
+          genreArray.push(`Action`);
+          break;
+        case 12:
+          genreArray.push(`Aventure`);
+          break;
+        case 16:
+          genreArray.push(`Animation`);
+          break;
+        case 35:
+          genreArray.push(`Comédie`);
+          break;
+        case 80:
+          genreArray.push(`Policier`);
+          break;
+        case 99:
+          genreArray.push(`Documentaire`);
+          break;
+        case 18:
+          genreArray.push(`Drame`);
+          break;
+        case 10751:
+          genreArray.push(`Famille`);
+          break;
+        case 14:
+          genreArray.push(`Fantasy`);
+          break;
+        case 36:
+          genreArray.push(`Histoire`);
+          break;
+        case 27:
+          genreArray.push(`Horreur`);
+          break;
+        case 10402:
+          genreArray.push(`Musique`);
+          break;
+        case 9648:
+          genreArray.push(`Mystère`);
+          break;
+        case 10749:
+          genreArray.push(`Romance`);
+          break;
+        case 878:
+          genreArray.push(`Science-fiction`);
+          break;
+        case 10770:
+          genreArray.push(`Téléfilm`);
+          break;
+        case 53:
+          genreArray.push(`Thriller`);
+          break;
+        case 10752:
+          genreArray.push(`Guerre`);
+          break;
+        case 37:
+          genreArray.push(`Western`);
+          break;
+        default:
+          break;
+      }
+    }
+    return genreArray.map((genre) => <li key={genre}>{genre}</li>);
+  };
+
+  useEffect(() => {
+    axios.get(API_GENRE_MOVIE + PUBLIC_KEY).then((res) => console.log(res.movie))
+  }, [])
 
   return (
     <div className="card">
@@ -20,9 +95,20 @@ const Card = ({ movie }) => {
       />
       <h2>{movie.title}</h2>
       {movie.release_date ? (
-      <h5>Sortie le : {dateFormater(movie.release_date)}</h5>  
-     ): null}
-     <h4>{movie.vote_average}/10 <span>⭐</span></h4>
+        <h5>Sortie le : {dateFormater(movie.release_date)}</h5>
+      ) : null}
+      <h4>
+        {movie.vote_average}/10 <span>⭐</span>
+      </h4>
+      <ul>
+      {movie.genre_ids
+          ? genreFinder()
+          : movie.genres.map((genre) => <li key={genre}>{genre.name}</li>)}
+      </ul>
+      <h3>Synopsis</h3>
+      <p>{movie.overview}</p>
+
+      <div className="btn">Ajouter aux coups de coeur</div>
     </div>
   );
 };
--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ export const API_MOVIE_URL = "https://api.themoviedb.org/3";
 
 export const API_KEY_MOVIE = `${API_MOVIE_URL}/movie/popular?api_key=`;
 
+export const API_GENRE_MOVIE = `${API_MOVIE_URL}/genre/movie/list?api_key=`;
+
 export const API_IMAGE = "https://image.tmdb.org/t/p/w500";
